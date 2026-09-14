635d8180d3afc3d3203e11e00482e06408e4d028
Marco Bueno <marcobbueno@gmail.com>
2026-09-14 18:07:50 -0300

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        size = len(prices)
+        profit = 0
+        menor = float('inf')
+        for p in prices:
+            if p < menor:
+                menor = p
+            if p - menor > profit:
+                profit = p -  menor
+        return profit
+
+
+# [10,1,5,6,7,1]
+
+
+            
+
